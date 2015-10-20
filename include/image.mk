f428b1df66d4069b1252c5b85d6aeda7b9fedf90
Nikita Nazarenko <nnazarenko@radiofid.ru>
2015-10-20 18:50:20 +0300

    image.mk: implement SysupgradeTAR for non-nand routers

--- include/image.mk
+++ include/image.mk
@@ -159,6 +159,16 @@ endef
 # $(1): board name
 # $(2): rootfs type
 # $(3): kernel image
+define Image/Build/SysupgradeTAR
+	mkdir -p "$(KDIR_TMP)/sysupgrade-$(1)/"
+	echo "BOARD=$(1)" > "$(KDIR_TMP)/sysupgrade-$(1)/CONTROL"
+	[ -z "$(2)" ] || $(CP) "$(KDIR)/root.$(2)" "$(KDIR_TMP)/sysupgrade-$(1)/root"
+	[ -z "$(3)" ] || $(CP) "$(3)" "$(KDIR_TMP)/sysupgrade-$(1)/kernel"
+	[ ! -d "$(TOPDIR)/env/preupgrade.d" ] || $(CP) "$(TOPDIR)/env/preupgrade.d/" "$(KDIR_TMP)/sysupgrade-$(1)/"
+	(cd "$(KDIR_TMP)"; $(TAR) cvf \
+		"$(BIN_DIR)/$(IMG_PREFIX)-$(1)-$(2)-sysupgrade.tar" sysupgrade-$(1))
+endef
+
 ifneq ($(CONFIG_NAND_SUPPORT),)
    define Image/Build/SysupgradeNAND
 	mkdir -p "$(KDIR_TMP)/sysupgrade-$(1)/"
