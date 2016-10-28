0f23b22230d5ad427b4c9fc30d1215f652dc29d4
Nikita Nazarenko <nnazarenko@radiofid.com>
2016-10-28 12:03:59 +0300

    at91: ruh2b/3 pre-initial image build

---
+++ target/linux/at91/legacy/profiles/irz.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2013 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/IRZ_RUH2B
+	NAME:=IRZ RUH2b
+endef
+define Profile/IRZ_RUH2B/Description
+	IRZ RUH2b router board
+endef
+$(eval $(call Profile,IRZ_RUH2B))
+
+define Profile/IRZ_RUH3
+	NAME:=IRZ RUH3
+endef
+define Profile/IRZ_RUH3/Description
+	IRZ RUH3 router board
+endef
+$(eval $(call Profile,IRZ_RUH3))
+
+
+
